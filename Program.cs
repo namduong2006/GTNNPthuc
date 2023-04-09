c48a4650397c6c96df451464358173f9158c2d2b
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-09 20:19:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTNNPThuc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+            Console.WriteLine("Số phần tử của mảng là :");
+            n=int.Parse(Console.ReadLine());
+            int[] list1 = new int [n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhập giá trị của phần tử thứ {i}");
+                list1[i] = int.Parse(Console.ReadLine());
+             }
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Dãy vừa nhập:");
+            for (int i=0; i<n;i++)
+            { Console.Write(  list1[i] + "  " ); }
+            int a = MinValue(list1);
+            Console.WriteLine();
+            Console.WriteLine($"Vị trí giá trị nhỏ nhất là {a+1} và có giá trị là {list1[a]}");
+            Console.ReadLine();
+        }
+        public static int MinValue(int[]list)
+        {
+            int min = list[0];
+            int t = 0;
+            for (int i = 1; i < list.Length; i++)
+            {
+                if (min > list[i])
+                {
+                    min = list[i];
+                    t = i;
+                }
+            }
+            return t;
+            
+
+        }
+    }
+}
